eb5d5e92e83896ca642ec045cc50800bee63b100
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-09-11 17:06:57 +0100

    Change nav background color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ export default function NavBar() {
             </ul>
             <div className="burger-menu" onClick={() => setIsOpen((prevState) => !prevState )}>&#9776;</div>
         </nav>
-        <h1 className={isOpen ? "navbar-open" : ""}>Hello world!</h1>
+        {/* <h1 className={isOpen ? "navbar-open" : ""}>Hello world!</h1> */}
         </>
     )
 }
