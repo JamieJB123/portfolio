f20a3a37247c53a3f3a8ed869972a1a3bdb57925
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-09-12 11:39:42 +0100

    Add content to the technologies cards. Remove the hard coded cards for cleaner code. Instead, create separate js file with content data and map over it to create the cards.

---
+++ cards.js
@@ -0,0 +1,58 @@
+export default [
+    {
+        id: 1,
+        image: '/src/assets/javascript.png',
+        alt: "JavaScript logo",
+        title: 'JavaScript',
+        content: 'Learning since 2025'
+    },
+    {
+        id: 2,
+        image: '/src/assets/python.png',
+        alt: 'Python logo',
+        title: 'Python',
+        content: 'Learning since 2025'
+    },
+    {
+        id: 3,
+        image: '/src/assets/database.png',
+        alt: 'Database logo',
+        title: 'PostgreSQL| MySQL',
+        content: 'Learning since 2025'
+    },
+    {
+        id: 4,
+        image: '/src/assets/django.png',
+        alt: 'Django logo',
+        title: 'Django',
+        content: 'Learning since 2025'
+    },
+    {
+        id: 5,
+        image: '/src/assets/react.png',
+        alt: 'React logo',
+        title: 'React.js',
+        content: 'Learning since 2025'
+    },
+    {
+        id: 6,
+        image: '/src/assets/css.png',
+        alt: 'CSS3 logo',
+        title: 'CSS3',
+        content: 'Learning since 2025'
+    },
+    {
+        id: 7,
+        image: '/src/assets/html.png',
+        alt: 'HTML5 logo',
+        title: 'HTML5',
+        content: 'Learning since 2025'
+    },
+    {
+        id: 8,
+        image: '/src/assets/tailwind.png',
+        alt: 'Tailwind CSS logo',
+        title: 'Tailwind CSS',
+        content: 'Learning since 2025'
+    }
+]
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,25 @@
+import cards from '../../cards'
+
 export default function About() {
+
+    const cardElements = cards.map((card) => {
+        return (
+            <div className="card-container w-64 h-80 perspective" id={card.id}>
+                <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
+                    {/* Front */}
+                    <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
+                        <img className="card-image" src={card.image} alt={card.alt}></img>
+                    </div>
+                    {/* Back */}
+                    <div className="back-side w-full h-full flex flex-col items-center justify-center backface-hidden rotate-y-180 ">
+                        <h3 className="card-title">{card.title}</h3>
+                        <p className="card-content">{card.content}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    })
+
     return (
         <>
             <main>
@@ -15,103 +36,8 @@ export default function About() {
                     <h2>Skills:</h2>
                     <p>Over the last several months I have been honing my skills as a software developer across the stack. With a background in psychology and social care, I bring with me the same attentiveness, care and focus on user experience.</p>
                 </section>
-                <section className="cards-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-6">
-                    <div className="card-container w-64 h-80 perspective">
-                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
-                            {/* Front */}
-                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
-                                Front of Card
-                            </div>
-                            {/* Back */}
-                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180 ">
-                                Backside of Card
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-container w-64 h-80 perspective">
-                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
-                            {/* Front */}
-                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
-                                Front of Card
-                            </div>
-                            {/* Back */}
-                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180">
-                                Backside of Card
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-container w-64 h-80 perspective">
-                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
-                            {/* Front */}
-                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
-                                Front of Card
-                            </div>
-                            {/* Back */}
-                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180">
-                                Backside of Card
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-container w-64 h-80 perspective">
-                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
-                            {/* Front */}
-                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
-                                Front of Card
-                            </div>
-                            {/* Back */}
-                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180">
-                                Backside of Card
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-container w-64 h-80 perspective">
-                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
-                            {/* Front */}
-                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
-                                Front of Card
-                            </div>
-                            {/* Back */}
-                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180">
-                                Backside of Card
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-container w-64 h-80 perspective">
-                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
-                            {/* Front */}
-                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
-                                Front of Card
-                            </div>
-                            {/* Back */}
-                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180">
-                                Backside of Card
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-container w-64 h-80 perspective">
-                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
-                            {/* Front */}
-                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
-                                Front of Card
-                            </div>
-                            {/* Back */}
-                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180">
-                                Backside of Card
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-container w-64 h-80 perspective">
-                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
-                            {/* Front */}
-                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
-                                Front of Card
-                            </div>
-                            {/* Back */}
-                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180">
-                                Backside of Card
-                            </div>
-                        </div>
-                    </div>
+                <section className="cards-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 py-10">
+                    {cardElements}
                 </section>
             </main>
 
