42c55aed64625b2a997360c13b5e64a4ff74d92e
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-09-12 10:49:07 +0100

    Install tailwind for layout responsiveness. Create cards section with placeholder styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,6 +15,104 @@ export default function About() {
                     <h2>Skills:</h2>
                     <p>Over the last several months I have been honing my skills as a software developer across the stack. With a background in psychology and social care, I bring with me the same attentiveness, care and focus on user experience.</p>
                 </section>
+                <section className="cards-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-6">
+                    <div className="card-container w-64 h-80 perspective">
+                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
+                            {/* Front */}
+                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
+                                Front of Card
+                            </div>
+                            {/* Back */}
+                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180 ">
+                                Backside of Card
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-container w-64 h-80 perspective">
+                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
+                            {/* Front */}
+                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
+                                Front of Card
+                            </div>
+                            {/* Back */}
+                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180">
+                                Backside of Card
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-container w-64 h-80 perspective">
+                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
+                            {/* Front */}
+                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
+                                Front of Card
+                            </div>
+                            {/* Back */}
+                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180">
+                                Backside of Card
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-container w-64 h-80 perspective">
+                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
+                            {/* Front */}
+                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
+                                Front of Card
+                            </div>
+                            {/* Back */}
+                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180">
+                                Backside of Card
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-container w-64 h-80 perspective">
+                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
+                            {/* Front */}
+                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
+                                Front of Card
+                            </div>
+                            {/* Back */}
+                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180">
+                                Backside of Card
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-container w-64 h-80 perspective">
+                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
+                            {/* Front */}
+                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
+                                Front of Card
+                            </div>
+                            {/* Back */}
+                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180">
+                                Backside of Card
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-container w-64 h-80 perspective">
+                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
+                            {/* Front */}
+                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
+                                Front of Card
+                            </div>
+                            {/* Back */}
+                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180">
+                                Backside of Card
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-container w-64 h-80 perspective">
+                        <div className="card relative w-full h-full transition-transform duration-700 transform-style hover:rotate-y-180">
+                            {/* Front */}
+                            <div className="front-side absolute w-full h-full flex items-center justify-center backface-hidden">
+                                Front of Card
+                            </div>
+                            {/* Back */}
+                            <div className="back-side w-full h-full flex items-center justify-center backface-hidden rotate-y-180">
+                                Backside of Card
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </main>
 
         </>
