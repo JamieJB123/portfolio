e5f40374fb86c22f275a98755d5762ebf6648500
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-09-12 16:12:27 +0100

    Add portfolio section skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
+import Portfolio from './components/Portfolio'
 
 export default function App() {
 
@@ -10,6 +11,7 @@ export default function App() {
       <Navbar />
       <Home />
       <About />
+      <Portfolio />
     </>
   )
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,22 @@
 export default function Portfolio() {
     return (
-        
+        <>
+        <section id="portfolio" className="portfolio-section flex flex-col items-center">
+            <div className="portfolio-title flex justify-center items-center gap-4">
+                <hr className="w-1/3"/>
+                <h2 className="text-5xl">Portfolio</h2>
+                <hr className="w-1/3"/>
+            </div>
+            <ul className="flex flex-col">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
+        </>
     )
 }
