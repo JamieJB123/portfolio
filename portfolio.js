810bb9b59a311c7a0711bb4c2f8da77ce507e2a7
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-09-13 18:00:57 +0100

    Add portfolio images. Hard code some of portfolio section. Add a portfolio data file in order to be able to more cleanly render portfolio section

---
+++ portfolio.js
@@ -0,0 +1,59 @@
+export default [
+    {
+        id: 1,
+        image: '/src/assets/portfolio/assembly-endgame.jpg',
+        alt: 'Portfolio image of assembly endgame react project',
+        title: "Assembly Endgame",
+        content: "Practice project to strengthen my understanding of core React concepts.",
+        liveLink: "https://jamiejb123.github.io/react-assembly-endgame/",
+        languages: ["HTML", "CSS", "JavaScript",],
+        frameworks: ["React"]
+    },
+    {
+        id: 2,
+        image: "/src/assets/portfolio/recipe-maker.jpg",
+        alt: "Portfolio image of AI integrated react recipe maker app",
+        title: "Recipe Maker",
+        content: "Practice project, including AI model integration, to strengthen React understanding.",
+        languages: ["HTML", "CSS", "JavaScript",],
+        frameworks: ["React"]
+    },
+    {
+        id: 3,
+        image: "/src/assets/portfolio/companion-app.jpg",
+        alt: "Portfolio image of Django Companion App",
+        title: "Companion App",
+        content: "Companion app idea sketch built for vulnerable users using Django framework.",
+        liveLink: "https://hackathon2-helper-182de8322106.herokuapp.com/",
+        languages: ["HTML", "CSS", "JavaScript", "Python"],
+        frameworks: ["Django", "Bootstrap"]
+    },
+    {
+        id: 4,
+        image: "/src/assets/portfolio/chaos-cards.jpg",
+        alt: "Portfolio image of Django Chaos Cards App",
+        title: "Chaos Cards",
+        content: "Chaos Cards is a game I built as a project during my Code Institute Bootcamp using Django framework.",
+        liveLink: "https://chaos-cards-de954de9fb2a.herokuapp.com/",
+        languages: ["HTML", "CSS", "JavaScript", "Python",],
+        frameworks: ["Django", "Bootstrap"]
+    },
+    {
+        id: 5,
+        image: "/src/assets/portfolio/dirty-water.jpg",
+        alt: "Portfolio image of Dirty Water Project",
+        title: "Dirty Water",
+        content: "Dirty water was an environmental app displaying live sewage discharge and tidal data on an interactive map. No live link because API keys were exposed as this was a pure front-end project.",
+        languages: ["HTML", "CSS", "JavaScript"],
+        frameworks: ["Bootstrap"]
+    },
+    {
+        id: 6,
+        image: "/src/assets/portfolio/healthy-mind.jpg",
+        alt: "Portfolio image of Healthy Mind Project",
+        title: "Healthy Mind",
+        content: "First project independently built as part of my Code Institute bootcamp. This was the first website I independently built.",
+        languages: ["HTML", "CSS", "JavaScript"],
+        frameworks: ["Bootstrap"]
+    }
+]
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,21 +1,83 @@
 export default function Portfolio() {
     return (
         <>
-        <section id="portfolio" className="portfolio-section flex flex-col items-center">
-            <div className="portfolio-title flex justify-center items-center gap-4">
+        <section id="portfolio" className="portfolio-section flex flex-col items-center md:grid grid-cols-[4fr_6fr] gap-4">
+            <div className="portfolio-title flex justify-center items-center gap-4 ps-4">
                 <hr className="w-1/3"/>
                 <h2 className="text-5xl">Portfolio</h2>
                 <hr className="w-1/3"/>
             </div>
-            <ul className="flex flex-col">
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
+            <div className="list-container p-20 ps-30">
+                <ul className="flex flex-col gap-8">
+                    <li>
+                        <div className="flex items-center gap-6">
+                            <div className="portfolio-image-wrapper">
+                                <img src="/src/assets/portfolio/assembly-endgame.jpg" alt="Portfolio image of Assembly Endgame React project" />
+                            </div>
+                            <div className="portfolio-info-wrapper">
+                                <h3>Assembly Endgame</h3>
+                                <a href="https://jamiejb123.github.io/react-assembly-endgame/" target="_blank" rel="no_opener">Live Link</a>
+                                <p>Practice project to strengthen my understanding of core React concepts.</p>
+                                <span>HTML</span>
+                                <span>CSS</span>
+                                <span>JavaScript</span>
+                                <span>React</span>
+                                Content
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="flex items-center">
+                            <div className="portfolio-image-wrapper">
+                                <img src="/src/assets/portfolio/recipe-maker.jpg" alt="Portfolio image of AI integrated recipe maker React project" />
+                            </div>
+                            <div className="portfolio-info-wrapper">
+                                Content
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="flex items-center">
+                            <div className="portfolio-image-wrapper">
+                                <img src="/src/assets/portfolio/companion-app.jpg" alt="" />
+                            </div>
+                            <div className="portfolio-info-wrapper">
+                                Content
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="flex items-center">
+                            <div className="portfolio-image-wrapper">
+                                <img src="/src/assets/portfolio/chaos-cards.jpg" alt="" />
+                            </div>
+                            <div className="portfolio-info-wrapper">
+                                Content
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="flex items-center">
+                            <div className="portfolio-image-wrapper">
+                                <img src="/src/assets/portfolio/dirty-water.jpg" alt="" />
+                            </div>
+                            <div className="portfolio-info-wrapper">
+                                Content
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="flex items-center">
+                            <div className="portfolio-image-wrapper">
+                                <img src="/src/assets/portfolio/healthy-mind.jpg" alt="" />
+                            </div>
+                            <div className="portfolio-info-wrapper">
+                                Content
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </section>
         </>
     )
