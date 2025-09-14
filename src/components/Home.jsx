9294ebc6b796958f8990dc952e5cd0bbd27aecc7
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-09-14 16:18:14 +0100

    Install framer motion into project dependencies and implement sexy progress bar animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,27 @@
 import Typing from './Typing'
+import { motion, useScroll, useTransform } from 'motion/react'
 
 export default function Home() {
+
+    const { scrollYProgress } = useScroll()
+    const background = useTransform(
+        scrollYProgress,
+        [0, 0.5, 1],
+        [ "#312E81", "#9333EA", "#EC4899" ]
+    )
+
     return (
         <>
             <header id="home" className="header-section">
+                <motion.div
+                    className="progress-bar"
+                    style={{
+                        transformOrigin: "left",
+                        scaleX: scrollYProgress,
+                        background: background,
+                    }}
+
+                />
                 <Typing />
                 <hr></hr>
                 <div className="glow"></div>
