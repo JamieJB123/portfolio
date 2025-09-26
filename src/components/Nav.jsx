6bc47e36789f337d58264172272d1a2982a42de4
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-09-26 14:57:58 +0100

    Tweak layout of navbar to avoid odd layout quirks on safari mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -18,16 +18,16 @@ export default function NavBar() {
                 >
                     &#9776;
                 </button>
+                <ul
+                    id="main-menu"
+                    className={`nav-links ${isOpen ? "active" : ""}`}
+                >
+                    <li><a onClick={() => setIsOpen(false)} href="#home">Home</a></li>
+                    <li><a onClick={() => setIsOpen(false)} href="#about">About</a></li>
+                    <li><a onClick={() => setIsOpen(false)} href="#portfolio">Portfolio</a></li>
+                    <li><a onClick={() => setIsOpen(false)} href="#contact">Contact</a></li>
+                </ul>
             </div>
-            <ul
-                id="main-menu"
-                className={`nav-links ${isOpen ? "active" : ""}`}
-            >
-                <li><a onClick={() => setIsOpen(false)} href="#home">Home</a></li>
-                <li><a onClick={() => setIsOpen(false)} href="#about">About</a></li>
-                <li><a onClick={() => setIsOpen(false)} href="#portfolio">Portfolio</a></li>
-                <li><a onClick={() => setIsOpen(false)} href="#contact">Contact</a></li>
-            </ul>
         </nav>
     )
 }
